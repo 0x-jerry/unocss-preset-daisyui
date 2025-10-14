ce515cb23bd6aba916e807e886dea2dade85ee33
Jerry <x.jerry.wang@gmail.com>
2025-10-14 07:23:45 +0000

    chore: fix test workflow

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,17 @@
-import { parse } from 'css-tree'
-import { readFile } from 'node:fs/promises'
-import { fileURLToPath } from 'node:url'
-import { getAllClassNames } from '../src/utils'
-import { createRequire } from 'node:module'
+import { parse } from "css-tree";
+import { readFile } from "node:fs/promises";
+import { createRequire } from "node:module";
+import { getAllClassNames } from "../src/utils";
 
-describe('utils', () => {
-  it('get class names', async () => {
-    const filePath = createRequire(import.meta.url).resolve(
-      'daisyui/daisyui.css',
-    )
+describe("utils", () => {
+	it("get class names", async () => {
+		const filePath = createRequire(import.meta.url).resolve(
+			"daisyui/daisyui.css",
+		);
 
-    const names = getAllClassNames(parse(await readFile(filePath, 'utf-8')))
-    expect(names).matchSnapshot()
-  })
-})
+		const names = getAllClassNames(
+			parse(await readFile(filePath, "utf-8")),
+		).toSorted();
+		expect(names).matchSnapshot();
+	});
+});
